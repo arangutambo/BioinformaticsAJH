715d9ce6050aeddec0983f4efd71426b61fba7cc
arangutambo <arangutambo@gmail.com>
2025-03-27 17:52:30 +1000

    Problem 4 Solution

--- RosalindProblems/4 Mendel's First Law/Solution 4.1.py
+++ RosalindProblems/4 Mendel's First Law/Solution 4.1.py
@@ -1,6 +1,20 @@
 import pyperclip
 
+DataSet = open("/Users/antoinejhaddad/Downloads/Mendel's First Law.txt", "r")
+DataSetRead = DataSet.read()
+OrganismAmounts = DataSetRead.split(" ")
 
+k = int(OrganismAmounts[0])
+m = int(OrganismAmounts[1])
+n = int(OrganismAmounts[2])
+
+N = k + m + n
+
+ProbabilityDominant = (1/(N*(N-1)))*(k*((k-1)+m+n)+m*(k+(m-1)*0.75+n*0.5)+n*(k + m*0.5))
+
+pyperclip.copy(ProbabilityDominant)
+
+DataSet.close()
 
 
 
